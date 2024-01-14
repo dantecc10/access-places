0f8e81697b7afdc708c783516b2adc9edd18a6bc
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-01-14 15:03:10 -0600

    ..

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - Accesos Inclusivos</title>
+    <title>Inicio - Accesos Inclusivos</title>
     <meta name="twitter:description" content="Sitio web para encontrar accesos inclusivos en Chignahuapan, Puebla.">
     <meta name="twitter:image" content="https://teacherdhapps.castelancarpinteyro.com/assets/img/logo.png">
     <meta property="og:type" content="website">
